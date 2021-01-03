8abcbf786e67c8c5ef6b2adeeffa3c5f0c52dcc1
Oliver Kelton <oakelton@gmail.com>
2021-01-02 18:05:40 -0800

    defined query_SCONF_ACCEPT_ENCODING in fm.h; used it in url.c/otherinfo()

--- fm.h
+++ fm.h
@@ -281,6 +281,7 @@ extern int REV_LB[];
 #define SCONF_N_FIELD		7
 #define query_SCONF_SUBSTITUTE_URL(pu) ((const char *)querySiteconf(pu, SCONF_SUBSTITUTE_URL))
 #define query_SCONF_USER_AGENT(pu) ((const char *)querySiteconf(pu, SCONF_USER_AGENT))
+#define query_SCONF_ACCEPT_ENCODING(pu) ((const char *)querySiteconf(pu, SCONF_ACCEPT_ENCODING))
 #define query_SCONF_URL_CHARSET(pu) ((const wc_ces *)querySiteconf(pu, SCONF_URL_CHARSET))
 #define query_SCONF_NO_REFERER_FROM(pu) ((const int *)querySiteconf(pu, SCONF_NO_REFERER_FROM))
 #define query_SCONF_NO_REFERER_TO(pu) ((const int *)querySiteconf(pu, SCONF_NO_REFERER_TO))
